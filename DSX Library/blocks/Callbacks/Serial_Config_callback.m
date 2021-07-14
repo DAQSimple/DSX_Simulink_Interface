6bd57cf21104503885796dab66cc4c8079f69246
Harjaapb <hsbharjaap@gmail.com>
2021-07-13 22:22:03 -0700

    Digital Read working
    - Changed how the loc value was being set
    - Set the default baud rate to 230400
    - Cleaned up the test model

--- DSX Library/blocks/Callbacks/Serial_Config_callback.m
+++ DSX Library/blocks/Callbacks/Serial_Config_callback.m
@@ -1,6 +1,6 @@
 function Serial_Config_callback(command,com,baud)
 %% Input checks
-initBaud = 256000;
+initBaud = 230400;
 timeOutString = 'DSX.Timeout = 1';
 [ports,numports] = Serial_Get_Ports();  % get list of all com ports
 likely_com = ports(numports);           % guess that the device is the last com port

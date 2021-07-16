e724a044638ad29ac8cb940ff500dce22a1c352e
tsaxon <tsaxon@sfu.ca>
2021-07-15 23:06:33 -0700

    Serial Timeout From 1s to 1ms

--- DSX Library/blocks/Callbacks/Serial_Config_callback.m
+++ DSX Library/blocks/Callbacks/Serial_Config_callback.m
@@ -1,7 +1,7 @@
 function Serial_Config_callback(command,com,baud)
 %% Input checks
 initBaud = 230400;
-timeOutString = 'DSX.Timeout = 1';
+timeOutString = 'DSX.Timeout = 1e-3';
 [ports,numports] = Serial_Get_Ports();  % get list of all com ports
 likely_com = ports(numports);           % guess that the device is the last com port
 %% Check globals
